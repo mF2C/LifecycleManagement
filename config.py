10ad5cab288ba4bcaec4d7c192e10e5100f0c722
Administrator <ari-ct@lists.atosresearch.eu>
2019-06-06 16:13:02 +0200

    version 1.2.7

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ Created on 18 oct. 2018
 
 #!/usr/bin/python
 
-dic = { "VERSION":                      "1.2.6",
+dic = { "VERSION":                      "1.2.7",
 
         # LIFECYCLE MANAGER MODE: "DEFAULT", "MF2C" , "STANDALONE"
         "LM_MODE":                      "MF2C",
